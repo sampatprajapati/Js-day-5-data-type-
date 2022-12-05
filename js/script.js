6cac73266a0dbf9b12764167fdc98a41be6fbaa4
Sampat Prajapati <sampatprajapati@gmail.com>
2022-12-06 00:44:26 +0530

    data type js

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var n2 = {
+    w1: `Tips `, //Property:value
+    w2: `to `,
+    w3: `overcome `, //using ``backtick for string
+    w4: `your `,
+    w5: `inner `,
+    w6: `battles `,
+    w7: `and `,
+    w8: `achieve `,
+    w9: `family `,
+    w10: `harmony. `,
+};
+var news2 =
+    n2.w1 +
+    n2.w2 +
+    n2.w3 +
+    n2.w4 +
+    n2.w5 +
+    n2.w6 +
+    n2.w7 +
+    n2.w8 +
+    n2.w9 +
+    n2.w10;
+window.alert(`News-2 ` + news2); //2 way to see the js output
+
+let n5 = {
+    w1: "Unique ",
+    w2: "places ",
+    w3: "to ",
+    w4: "visit ",
+    w5: "during ",
+    w6: "winter ",
+    w7: "holidays ",
+    w8: "J&K's ",
+    w9: "Bandipora ",
+    w10: "hosts ",
+    w11: "the ",
+    w12: "first-ever ",
+    w13: "Tribal ",
+    w14: "Winter ",
+    w15: "Festival. ",
+};
+var news5 =
+    n5.w1 +
+    n5.w2 +
+    n5.w3 +
+    n5.w4 +
+    n5.w5 +
+    n5.w6 +
+    n5.w7 +
+    n5.w8 +
+    n5.w9 +
+    n5.w10 +
+    n5.w11 +
+    n5.w12 +
+    n5.w13 +
+    n5.w14 +
+    n5.w15;
